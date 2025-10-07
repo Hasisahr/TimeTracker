cc7d112eb861eb010a9d57c6177b43df1a719bcb
Antonio <150443704+Hasisahr@users.noreply.github.com>
2025-10-07 13:13:54 +0200

    Add files via upload

---
+++ TimeTrackerManagerDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TimeTracker.Model;
+
+namespace TimeTracker.DAL
+{
+    public class TimeTrackerManagerDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<Salary> Salaries { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<ScheduleAssignment> ScheduleAssignments { get; set; }
+
+        public TimeTrackerManagerDbContext(DbContextOptions<TimeTrackerManagerDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(l => new { l.LoginProvider, l.ProviderKey });
+            modelBuilder.Entity<IdentityUserRole<string>>().HasKey(r => new { r.UserId, r.RoleId });
+            modelBuilder.Entity<IdentityUserToken<string>>().HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+
+            modelBuilder.Entity<Schedule>()
+                .HasMany(s => s.Assignments)
+                .WithOne(a => a.Schedule)
+                .HasForeignKey(a => a.ScheduleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<Salary>()
+                .HasOne<IdentityUser>()
+                .WithMany()
+                .HasForeignKey(s => s.IdentityUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            modelBuilder.Entity<ScheduleAssignment>()
+                .Property(a => a.Position)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Salary>()
+                .HasIndex(s => s.IdentityUserId);
+        }
+        }
+    }
